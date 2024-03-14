96ba234f3f0d88a1c6942f686f27f79058368891
Миша <malorodovmihail009@gmail.com>
2024-03-14 18:21:33 +0300

    fragment commit1

--- app/src/main/java/com/example/practice4/MainActivity.java
+++ app/src/main/java/com/example/practice4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.practice4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,34 +12,22 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public EditText main_surname, main_name;
-    private Button main_next_btn;
+    private FragmentManager manager;
+    private FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        main_next_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(main_surname.getText() == null || main_name.getText() == null)
-                {
-                    Toast.makeText(MainActivity.this, "Введите имя и фамилию", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Intent intent = new Intent(MainActivity.this, Activity2.class);
-                    intent.putExtra("surname", main_surname.getText().toString());
-                    intent.putExtra("name", main_name.getText().toString());
-                    startActivity(intent);
-                }
-            }
-        });
-    }
+        manager = getSupportFragmentManager();
+        FirstFragment fragment1 = new FirstFragment();
+        fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment1, "fragment1");
+        fragmentTransaction.commit();
 
-    private void init()
-    {
-        main_surname = findViewById(R.id.main_surname);
-        main_name = findViewById(R.id.main_name);
-        main_next_btn = findViewById(R.id.main_next_btn);
+//        manager = getSupportFragmentManager();
+//        fragmentTransaction = manager.beginTransaction();
+//        fragmentTransaction.add(R.id.frame_second_fragment, new SecondFragment(), null);
+//        fragmentTransaction.commit();
+
     }
 }
